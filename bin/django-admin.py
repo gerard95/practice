82e520528dd24af6eae3302895737a3b4067a116
Cobb Lucas G <x86407@usmawkddth17613.eecs.usma.edu>
2017-02-24 19:17:04 -0500

    Adding Django

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/stu/cadet08/x86407/Desktop/it394/PracticeApp/PracticeApp/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
